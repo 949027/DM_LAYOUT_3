fe4995911b60677a266aa0e740d3b249f33f250d
949027 <949027@gmail.com>
2021-10-12 12:17:28 +0700

    Download books from tululu.org

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import requests
+import os
 
 
-url = "https://tululu.org/txt.php?id=32168"
+os.makedirs('books', exist_ok=True)
 
-response = requests.get(url)
-response.raise_for_status()
+for id_book in range(1, 11):
+    url = 'https://tululu.org/txt.php?id=' + str(id_book)
 
-filename = 'book.txt'
-with open(filename, 'wb') as file:
-    file.write(response.content)
+    response = requests.get(url)
+    response.raise_for_status()
+
+    filename = 'id' + str(id_book) + '.txt'
+    with open('books/' + filename, 'wb') as file:
+        file.write(response.content)
