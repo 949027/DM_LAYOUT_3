44ee5e8d33b8099a86231bfbbd3a83b4a14c2bde
949027 <949027@gmail.com>
2021-10-12 12:50:43 +0700

    Added check for redirect

--- main.py
+++ main.py
@@ -2,14 +2,28 @@ import requests
 import os
 
 
-os.makedirs('books', exist_ok=True)
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError
 
-for id_book in range(1, 11):
-    url = 'https://tululu.org/txt.php?id=' + str(id_book)
 
-    response = requests.get(url)
-    response.raise_for_status()
+def main():
+    os.makedirs('books', exist_ok=True)
 
-    filename = 'id' + str(id_book) + '.txt'
-    with open('books/' + filename, 'wb') as file:
-        file.write(response.content)
+    for id_book in range(1, 11):
+        url = 'https://tululu.org/txt.php?id=' + str(id_book)
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        try:
+            check_for_redirect(response)
+            filename = 'id' + str(id_book) + '.txt'
+            with open('books/' + filename, 'wb') as file:
+                file.write(response.content)
+        except requests.HTTPError:
+            pass
+
+
+if __name__ == '__main__':
+    main()
